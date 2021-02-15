a3f7dacfc89ac1c309ef28652514359ba2cbe8a5
Sebaschb36 <sebaschb36@gmail.com>
2021-02-15 15:16:51 -0600

    Proyecto inicial Spring boot con dependencias basicas

---
+++ src/test/java/Owlearning/OwlApp/OwlAppApplicationTests.java
@@ -0,0 +1,13 @@
+package Owlearning.OwlApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OwlAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
